686b68c56e60aa204665fb255b15cc63056bb63e
Dragon2000v <shmatko064@gmail.com>
2023-07-26 17:31:13 +0300

    Добавьте файлы проекта.

---
+++ ToDo/Models/Status.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoDemo.Models
+{
+    public class Status
+    {
+      
+        public string StatusId { get; set; } = string.Empty;
+        
+        public string Name { get; set; } = string.Empty;
+
+    }
+}
